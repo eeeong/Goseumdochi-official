e7a5a7c2d28b24e7598eb67e7a8e4c2b78943a96
조하은 <nancy323@naver.com>
2024-05-19 18:18:29 +0900

    #add 연동을 위한 config 설정 추가

---
+++ src/main/java/com/midas/goseumdochi/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.midas.goseumdochi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 허용할 origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*"); // 허용할 헤더
+    }
+}

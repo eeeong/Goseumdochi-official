f470da23bb28a9f8306d7c97ab9eecba3f1d15b1
hyoju <nicehyoju@gmail.com>
2024-05-25 16:16:01 +0900

    #add 커뮤니티

--- src/main/fronted/src/components/community.js
+++ src/main/fronted/src/components/community.js
@@ -6,13 +6,13 @@ return (
         <div id="App">
             <div id="community_header">
                 <div id="menu_btn"/>
+                <div id="home_btn"/>
                 <div id="title">
                     <img src={logo}/>
                     <h2>고슴도치 커뮤니티</h2>
                 </div>
-                <div id="home_btn"/>
             </div>
-            <div id="menu_community"
+            <div id="menu_community">
                 <ul>
                     <li><a>자유 게시판</a></li>
                     <li><a>HOT 게시물</a></li>
@@ -25,10 +25,28 @@ return (
             </div>
             <div id="contents_community">
                 <div id="header_contents_community">
+                    <div id="findsomething">
+                        <input type="text" placeholder="제목" id="find_title"/>
+                        <input type="submit" value="검색" id="find_btn"/>
+                        <div id="write_btn">
+                            <a>글쓰기</a>
+                        </div>
+                    </div>
                 </div>
                 <div id="contents_contents_community">
+                    <div id="letter_contents_community">
+
+                    </div>
                 </div>
                 <div id="footer_contents_community">
+                    <div id="back_write_btn">
+                        <a>이전</a>
+                    </div>
+                    <div id="next_write_btn">
+                        <a>다음</a>
+                    </div>
+                    <div id="page_number">
+                    </div>
                 </div>
             </div>
         </div>
